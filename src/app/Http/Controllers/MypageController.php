5225c601efa26aff34e10249bf3939c926061416
hashimoto takumi <takumih6013@gmail.com>
2024-06-19 19:55:09 +0900

    プロフィール画面の編集

--- src/app/Http/Controllers/MypageController.php
+++ src/app/Http/Controllers/MypageController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Item;
 
 class MypageController extends Controller
 {
     public function mypageIndex()
     {
-        return view('user.mypage');
+        $profiles = Item::all();
+        return view('user.mypage', compact('profiles'));
     }
 
     public function update()
--- src/resources/views/user/mypage.blade.php
+++ src/resources/views/user/mypage.blade.php
@@ -5,21 +5,29 @@
 @endsection
 
 @section('content')
-<h1>ユーザー名</h1>
-<figure>
-    <img src="" alt="ユーザーのプロフィール画像">
-</figure>
-<p>
-    <a href="">
-        プロフィールを編集
-    </a>
-</p>
-<section>
-    <h2>出品した商品</h2>
+<section class="mypage">
+    <div class="mypage__inner">
+        <div class="mypage__header">
+            <h1 class="mypage__title">ユーザー名</h1>
+            <a class="mypage__link" href="#">プロフィールを編集</a>
+        </div>
+        <div class="mypage__content">
+            <div class="mypage__content-link">
+                <a class="mypage__content-link-item" href="">出品した商品</a>
+                <a class="mypage__content-link-item mypage__content-link-item--purchase" href="">購入した商品</a>
+            </div>
+            <div class="mypage__card w__inner">
+                <div class="mypage__card-layout">
+                    @foreach($profiles as $profile)
+                    <figure class="mypage__card-img">
+                        <a class="mypage__card-link" href="#">
+                            <img class="mypage__card-link-item" src="{{ asset('storage/'.$profile->image_url) }}" alt="商品の画像" width="150" height="150">
+                        </a>
+                    </figure>
+                    @endforeach
+                </div>
+            </div>
+        </div>
+    </div>
 </section>
-<section>
-    <h2>購入した商品</h2>
-</section>
-
-
 @endsection
