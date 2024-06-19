214dc790fc36a074e66ddf49e04cd64d2f2928ce
hashimoto takumi <takumih6013@gmail.com>
2024-06-19 21:45:30 +0900

    プロフィールのビューの編集

--- src/resources/views/user/profile.blade.php
+++ src/resources/views/user/profile.blade.php
@@ -5,24 +5,39 @@
 @endsection
 
 @section('content')
-<h1>ユーザー名</h1>
-<form action="">
-    @csrf
-    <dl>
-        <dt>ここに画像</dt>
-        <dd><input type="file"></dd>
-        <dt>ユーザー名</dt>
-        <dd><input type="text"></dd>
-        <dt>郵便番号</dt>
-        <dd><input type="text"></dd>
-        <dt>住所</dt>
-        <dd><input type="text"></dd>
-        <dt>建物名</dt>
-        <dd><input type="text"></dd>
-    </dl>
-    <button>
-        更新する
-    </button>
-
-</form>
+<section class="profile">
+    <div class="profile__inner w__inner">
+        <h1 class="profile__title">プロフィール設定</h1>
+        <form class="profile-form" action="">
+            @csrf
+            <div class="profile-form__content">
+                <p class="profile-form__image">
+                    <label class="profile-form__label-image" for="image">画像を選択する</label>
+                    <input class="profile-form__input profile-form__input--image" type="file" id="image">
+                </p>
+                <div class="profile-form__content-layout">
+                    <p class="profile-form__text">
+                        <label class="profile-form__label" for="name">ユーザー名</label>
+                        <input class="profile-form__input" type="text" id="name">
+                    </p>
+                    <p class="profile-form__text">
+                        <label class="profile-form__label" for="postcode">郵便番号</label>
+                        <input class="profile-form__input" type="text" id="postcode">
+                    </p>
+                    <p class="profile-form__text">
+                        <label class="profile-form__label" for="address">住所</label>
+                        <input class="profile-form__input" type="text" id="address">
+                    </p>
+                    <p class="profile-form__text">
+                        <label class="profile-form__label" for="building">建物名</label>
+                        <input class="profile-form__input" type="text" id="building">
+                    </p>
+                </div>
+                <div class="profile-form__button">
+                    <button class="profile-form__button-submit">更新する</button>
+                </div>
+            </div>
+        </form>
+    </div>
+</section>
 @endsection
