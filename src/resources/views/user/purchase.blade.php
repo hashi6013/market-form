3f1d1263d05020e13b23dfc1388ec25182b0a2ca
hashimoto takumi <takumih6013@gmail.com>
2024-06-20 23:10:56 +0900

    購入ページの編集

--- src/resources/views/user/purchase.blade.php
+++ src/resources/views/user/purchase.blade.php
@@ -5,32 +5,53 @@
 @endsection
 
 @section('content')
-
-<h1>{{ $purchase->name }}</h1>
-<p>&yen;{{ $purchase->price }}</p>
-<figure>
-    <img src="{{ asset('storage/'.$purchase->image_url) }}" alt="購入する商品の画像">
-</figure>
-
-<form action="">
-    <section>
-        <h2>支払い方法</h2>
-        <a href="">変更する</a>
-    </section>
-    <section>
-        <h2>配送先</h2>
-        <a href="/purchase/address/{{$purchase->id}}">変更する</a>
-    </section>
-    <dl>
-        <dt>商品代金</dt>
-        <dd>&yen;{{ $purchase->price }}</dd>
-        <dt>支払金額</dt>
-        <dd>&yen;{{ $purchase->price }}</dd>
-        <dt>支払い方法</dt>
-        <dd>コンビニ払い</dd>
-    </dl>
-    <button>
-        購入する
-    </button>
-</form>
+<section class="purchase">
+    <div class="purchase__inner w__inner">
+        <form class="purchase-form" action="">
+            @csrf
+            <div class="purchase-form__item">
+                <div class="purchase-form__item-description">
+                    <h1 class="purchase-form__item-name">{{ $purchase->name }}</h1>
+                    <p class="purchase-form__item-price">&yen;{{ $purchase->price }}</p>
+                </div>
+                <figure>
+                    <img class="purchase-form__item-img" src="{{ asset('storage/'.$purchase->image_url) }}" alt="購入する商品の画像" width="100" height="110">
+                </figure>
+            </div>
+            <div class="purchase-form__payment">
+                <h2 class="purchase-form__payment-title">支払い方法</h2>
+                <p class="purchase-form__payment-item">
+                    <a class="purchase-form__payment-item-link" href="#">変更する</a>
+                </p>
+            </div>
+            <div class="purchase-form__address">
+                <h2 class="purchase-form__address-title">配送先</h2>
+                <p class="purchase-form__address-item">
+                    <a class="purchase-form__address-item-link" href="/purchase/address/{{$purchase->id}}">
+                        変更する
+                    </a>
+                </p>
+            </div>
+            <div class="purchase-form__button">
+                <button class="purchase-form__button-submit">購入する</button>
+            </div>
+            <div class="purchase-form__confirm">
+                <dl class="purchase-form__confirm-list">
+                    <div class="purchase-form__confirm-list-flex">
+                        <dt class="purchase-form__confirm-list-title">商品代金</dt>
+                        <dd class="purchase-form__confirm-list-description">&yen;{{ $purchase->price }}</dd>
+                    </div>
+                    <div class="purchase-form__confirm-list-flex">
+                        <dt class="purchase-form__confirm-list-title">支払い金額</dt>
+                        <dd class="purchase-form__confirm-list-description">&yen;{{ $purchase->price }}</dd>
+                    </div>
+                    <div class="purchase-form__confirm-list-flex">
+                        <dt class="purchase-form__confirm-list-title">支払い方法</dt>
+                        <dd class="purchase-form__confirm-list-description">コンビニ払い</dd>
+                    </div>
+                </dl>
+            </div>
+        </form>
+    </div>
+</section>
 @endsection
